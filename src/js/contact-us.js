7807a5d0d312bd953f738704b80db53b244ea6ad
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-10-06 08:53:12 +0200

    added js files

---
+++ src/js/contact-us.js
@@ -0,0 +1,8 @@
+function contactUs() {
+  function submit() {
+    console.log('js for contact us ajax submit');
+  }
+  submit();
+}
+
+contactUs();
---
+++ src/js/navbar.js
@@ -0,0 +1,8 @@
+function navbar() {
+  function smoothScrolling() {
+    console.log('js for navbar smooth scrolling');
+  }
+  smoothScrolling();
+}
+
+navbar();

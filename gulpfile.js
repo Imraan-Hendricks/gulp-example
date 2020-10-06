5e25e4e23d3c0cf0996568e9f0e9c0a57242ea1b
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-10-06 08:55:15 +0200

    added gulp task compileJs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+
+const compileJs = async () =>
+  gulp
+    .src('src/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
 
 const copyHtml = async () => gulp.src('src/*.html').pipe(gulp.dest('build'));
 
+exports.compileJs = compileJs;
 exports.copyHtml = copyHtml;

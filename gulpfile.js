b0a33adf7f69b9a428b35a5766ee1e0ab2cedc08
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-10-07 10:28:33 +0200

    added gulp task compileSass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
+const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 
 const compileJs = async () =>
@@ -9,7 +11,15 @@ const compileJs = async () =>
     .pipe(uglify())
     .pipe(gulp.dest('build/js'));
 
+const compileSass = async () =>
+  gulp
+    .src('src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('build/css'));
+
 const copyHtml = async () => gulp.src('src/*.html').pipe(gulp.dest('build'));
 
 exports.compileJs = compileJs;
+exports.compileSass = compileSass;
 exports.copyHtml = copyHtml;

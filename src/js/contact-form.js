240b792b0852cce0d673cd5c1a87833487339d90
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-10-26 10:02:43 +0200

    handled contact form request errors and validation feedback

--- src/js/contact-form.js
+++ src/js/contact-form.js
@@ -1,4 +1,8 @@
 const contactForm = () => {
+  const contactForm = document.getElementById('contactForm');
+  const form = Array.from(contactForm.children)
+    .filter((child) => (child.tagName === 'FORM'))[0];
+
   let data = {
     firstName: '',
     lastName: '',
@@ -7,9 +11,32 @@ const contactForm = () => {
   }
 
   const onChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value, nextElementSibling } = e.target;
     data[name] = value;
+ 
+    nextElementSibling.textContent = undefined; 
+    form.lastChild.textContent = undefined;
   };
+
+  const onErrors = (errs) => {
+    const inputGroups = Array.from(form.children)
+      .filter((formChild) => (formChild.tagName === 'DIV'));
+
+    errs.forEach((error) => {
+      if (error.param === 'general')
+        form.lastChild.textContent = error.message;
+      else
+        inputGroups.forEach((inputGroup) => {
+          let input = Array.from(inputGroup.children)
+            .filter((element) => (
+              element.tagName === 'INPUT' || element.tagName === 'TEXTAREA'
+            ))[0];
+
+          if(input.getAttribute('name') === error.param)
+            input.nextElementSibling.textContent = error.message; 
+        })        
+    })
+  }  
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -22,15 +49,21 @@ const contactForm = () => {
       if (this.readyState == 4 && this.status == 200) {
         const res = JSON.parse(this.responseText);
         console.log(res);
+
+        if(!res.success){
+          onErrors(res.err);
+          return;
+        }
+
+        console.log('handle success')
       }
     };
   
     xhr.send(JSON.stringify(data));
   }
   
-  const contactUsForm = document.getElementById('contactUsForm');
-  contactUsForm.addEventListener('change', onChange);
-  contactUsForm.addEventListener('submit', onSubmit);
+  form.addEventListener('change', onChange);
+  form.addEventListener('submit', onSubmit);
 };
 
 contactForm();
